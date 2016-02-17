e5d38f7667aa27a4d04756af5c37105bc72d6ab0
erkanceylan <erkanceylan4@gmail.com>
2016-02-17 15:20:56 +0200

    eko_index eklendi

---
+++ html/eko_index.php
@@ -0,0 +1,269 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Erkan Dijital Ticaret</title>
+  <link rel="stylesheet" href="bootstrap/css/bootstrap.css"/>
+</head>
+<body>
+  <div class="container">
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+<div class="navbar-header">
+  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+
+<a href="" class="navbar-brand" href="#"><span class="glyphicon glyphicon-star"></span>  Eko Ticaret  <span class="glyphicon glyphicon-star"></span></a>
+  </div>
+  <div id="navbar" class="navbar-collapse collapse">
+  <ul class="nav navbar-nav">
+<li class="active"><a href="#" >Home</a></li>
+<li><a href="#">About</a></li>
+<li><a href="#">Contact</a></li>
+<li><a href="#products">Products</a></li>
+<li><a href="#testimonials">Testimonials</a></li>
+<li class="dropdown">
+  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Sosyal Medya<span class="caret"></span></a>
+<ul class="dropdown-menu">
+<li><a href="#"><i class="fa fa-facebook"></i> Facebook</a></li>
+<li><a href="#"><i class="fa fa-twitter"></i> Twitter</a></li>
+<li><a href="#"><i class="fa fa-linkedin"></i> Linkedin</a></li>
+<li><a href="#"><i class="fa fa-github"></i> Github</a></li>
+  </ul>
+</li>
+    </ul>
+    <form class="navbar-form navbar-left" role="search">
+            <div class="form-group">
+              <input type="text" class="form-control" placeholder="Siteiçi Arama">
+            </div>
+            <button type="submit" class="btn btn-default">Ara</button>
+          </form>
+    <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">Giriş Yap</a></li>
+          </ul>
+        </div>
+        </div>
+    </nav>
+</div>
+  <div id="myCarousel" class="carousel slide" data-ride=carousel>
+<ol class="carousel-indicators">
+<li class="active" data-target="#myCarousel" data-slide-to="0"></li>
+<li data-target="#myCarousel" data-slide-to="1"></li>
+<li data-target="#myCarousel" data-slide-to="2"></li>
+<li data-target="#myCarousel" data-slide-to="3"></li>
+</ol>
+<div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img class="first-slide" src="http://www.lorempixel.com/1920/700" alt="Image"  >
+      <div class="container">
+      <div class="carousel-caption">
+      <h1>Örnek Yazı 1</h1>
+      <p>  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non, molestiae!</p>
+      </div>
+    </div>
+    </div>
+    <div class="item">
+  <img src="http://www.lorempixel.com/1920/700" alt="Image" class="second-slide" >
+       <div class="container">
+      <div class="carousel-caption">
+      <h1>Örnek Yazı 2</h1>
+      <p>  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia, consequatur.</p>
+      </div>
+    </div>
+  </div>
+  <div class="item">
+    <img src="http://www.lorempixel.com/1920/700" alt="Image" class="third-slide" >
+
+      <div class="container">
+      <div class="carousel-caption">
+      <h1>Örnek Yazı 3</h1>
+      <p>  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse, voluptas.</p>
+      </div>
+    </div>
+  </div>
+  <div class="item">
+    <img src="http://www.lorempixel.com/1920/700" alt="Image" class="fourth-slide" 66>
+      <div class="container">
+      <div class="carousel-caption">
+      <h1>Örnek Yazı 4</h1>
+      <p>  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse, voluptas.</p>
+      </div>
+      </div>
+    </div>
+</div>
+
+<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+  </div>
+<section id="products">
+  <div class="container">
+  <h3>Product Features</h3>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, reprehenderit?</p>
+<hr>
+
+<div class="row">
+  <div class="col-md-4">
+    <div class="media"><a href="#" class="pull-left">
+        <img class="thumbnail" src="http://lorempixel.com/120/120" alt="Image">
+      </a>
+      <div class="media-body">
+        <div class="pull-right">
+        <table class="table table-hover">
+          <tr>
+            <td><th>Product Name:</th></td>
+            <td>Lorem ipsum.</td>
+          </tr>
+          <tr>
+            <td><th>Product Date:</th></td>
+            <td>11-23-2014</td>
+          </tr>
+          <tr>
+            <td><th>Price:</th></td>
+            <td>19,99 $</td>
+          </tr>
+          <tr>
+            <td><th>Country:</th></td>
+            <td>U.S.A.</td>
+          </tr>
+        </table>
+          </div>
+      </div>
+        </div>
+  </div>
+
+  <div class="col-md-4">
+    <div class="media"><a href="#" class="pull-left">
+        <img class="thumbnail" src="http://lorempixel.com/120/120" alt="Image">
+      </a>
+      <div class="media-body">
+        <div class="pull-right">
+        <table class="table table-hover">
+          <tr>
+            <td><th>Product Name:</th></td>
+            <td>Lorem ipsum.</td>
+          </tr>
+          <tr>
+            <td><th>Product Date:</th></td>
+            <td>11-23-2014</td>
+          </tr>
+          <tr>
+            <td><th>Price:</th></td>
+            <td>19,99 $</td>
+          </tr>
+          <tr>
+            <td><th>Country:</th></td>
+            <td>U.S.A.</td>
+          </tr>
+        </table>
+          </div>
+      </div>
+        </div>
+  </div>
+  <div class="col-md-4">
+    <div class="media"><a href="#" class="pull-left">
+        <img class="thumbnail" src="http://lorempixel.com/120/120" alt="Image">
+      </a>
+      <div class="media-body">
+        <div class="pull-right">
+        <table class="table table-hover">
+          <tr>
+            <td><th>Product Name:</th></td>
+            <td>Lorem ipsum.</td>
+          </tr>
+          <tr>
+            <td><th>Product Date:</th></td>
+            <td>11-23-2014</td>
+          </tr>
+          <tr>
+            <td><th>Price:</th></td>
+            <td>19,99 $</td>
+          </tr>
+          <tr>
+            <td><th>Country:</th></td>
+            <td>U.S.A.</td>
+          </tr>
+        </table>
+          </div>
+      </div>
+        </div>
+  </div>
+
+  <div class="col-md-4">
+    <div class="media"><a href="#" class="pull-left">
+        <img class="thumbnail" src="http://lorempixel.com/120/120" alt="Image">
+      </a>
+      <div class="media-body">
+        <div class="pull-right">
+        <table class="table table-hover">
+          <tr>
+            <td><th>Product Name:</th></td>
+            <td>Lorem ipsum.</td>
+          </tr>
+          <tr>
+            <td><th>Product Date:</th></td>
+            <td>11-23-2014</td>
+          </tr>
+          <tr>
+            <td><th>Price:</th></td>
+            <td>19,99 $</td>
+          </tr>
+          <tr>
+            <td><th>Country:</th></td>
+            <td>U.S.A.</td>
+          </tr>
+        </table>
+          </div>
+      </div>
+        </div>
+  </div>
+
+
+</div>
+</div>
+<hr>
+</section>
+<section id="testimonials">
+  <div class="container">
+  <div class="panel">
+    <div class="panel-heading"><h3>Testimonials from our customers.</h3></div>
+    <div class="row">
+      <div class="col-md-6">
+        <blockquote>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, eos.<small>Emily from <cite>Nevada</cite></small></blockquote>
+      </div>
+      <div class="col-md-6">
+        <blockquote>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur, soluta.<small>Emily from <cite>Nevada</cite></small></blockquote>
+      </div>
+      <div class="col-md-6">
+        <blockquote>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur, soluta.<small>Emily from <cite>Nevada</cite></small></blockquote>
+      </div>
+      <div class="col-md-6">
+        <blockquote>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur, soluta.<small>Emily from <cite>Nevada</cite></small></blockquote>
+      </div>
+    </div>
+  </div>
+  </div>
+  <hr>
+</section>
+<sectin id="contact">
+  <div class="container">
+    <div class="well well-lg">
+      <h3 class="text-center">Contact Now !</h3>
+      <p class="text-center">Receive a great moments.</p>
+      <p class="text-center"><a href="" class="btn btn-success">Contact Place &rarr;</a></p>
+    </div>
+  </div>
+</sectin>
+  <script src="http://code.jquery.com/jquery.js"></script>
+  <script src="bootstrap/js/bootstrap.js"></script>
+</body>
+</html>
